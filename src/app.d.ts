9cadb54e974c2c4c732e78da1784c0c50a86838a
Pablo Camino <pablo@cloudflare.com>
2023-11-20 13:33:46 -0600

    s

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,6 +4,7 @@ declare global {
 	namespace App {
 		interface Locals {
 			auth: import('lucia').AuthRequest;
+			user: string | undefined
 		}
 		interface Platform {
 			env: {
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,11 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const auth = initializeLucia(event.platform?.env.DB);
+
 	event.locals.auth = auth.handleRequest(event);
+	if (event.locals?.auth) {
+		const Session = await event.locals.auth.validate();
+		event.locals.user = Session?.user.userId
+	}
 	return await resolve(event);
 };
