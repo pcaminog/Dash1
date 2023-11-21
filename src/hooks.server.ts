d92f546531b609b3d5e7f0658052f65a8b9d598b
Pablo Camino <pablo@cloudflare.com>
2023-11-20 21:59:15 -0600

    s

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,26 @@
+import { building } from '$app/environment';
 import { initializeLucia } from '$lib/server/lucia';
-import { redirect, type Handle } from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
+	if (building) {
+		const response = await resolve(event);
+		return response; // bailing here allows the 404 page to build
+	}
+
 	const auth = initializeLucia(event.platform?.env.DB);
 
 	event.locals.auth = auth.handleRequest(event);
 	event.locals.lucia = auth;
+
 	if (event.locals?.auth) {
 		const session = await event.locals.auth.validate();
-		if (!session) {
-			throw redirect(303, '/login?hooks_no_session');
-		}
+		// if (!session) {
+		// 	return new Response(null, {
+		// 		status: 307,
+		// 		headers: { location: '/login?hooks_no_session' }
+		// 	});
+		// }
 		event.locals.user = session?.user.userId;
 	}
 	return await resolve(event);
---
+++ src/routes/(noProtected)/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header" />
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter" />
+	<!-- (footer) -->
+</AppShell>
