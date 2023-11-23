41a3c648216ea0556a737f5be11988cfd300c905
Pablo Camino <pablo@cloudflare.com>
2023-11-22 21:52:01 -0600

    s

--- src/lib/components/ui/radio-group/radio-group-item.svelte
+++ src/lib/components/ui/radio-group/radio-group-item.svelte
@@ -15,6 +15,8 @@
 
 <RadioGroupPrimitive.Item
 	{value}
+	style="border-radius: 99999px "
+
 	class={cn(
 		"aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
 		className
