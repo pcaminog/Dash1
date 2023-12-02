19535d7b14ca6feef52e5e1d7b10cc793eb9e0f3
Pablo Camino <pablo@cloudflare.com>
2023-12-02 11:01:23 -0600

    s

--- src/routes/(logins)/logout/+server.ts
+++ src/routes/(logins)/logout/+server.ts
@@ -4,9 +4,9 @@ import { redirect } from '@sveltejs/kit';
 export const GET = async ({ locals }) => {
 	const session = await locals.auth.validate();
 	if (!session) {
-		locals.lucia.setSession(null);
+		locals.auth.setSession(null);
 	}
 	await locals.lucia.invalidateSession(session?.sessionId); // invalidate session
-	locals.lucia.setSession(null); // remove cookie
+	locals.auth.setSession(null); // remove cookie
 	throw redirect(302, '/login');
 };
