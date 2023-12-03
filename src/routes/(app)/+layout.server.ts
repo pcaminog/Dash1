f94d0f5e542bb199a5a7f9d1b4d2a773c9a25803
Pablo Camino <pablo@cloudflare.com>
2023-12-02 19:27:55 -0600

    s

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -2,11 +2,10 @@ import { API_URL } from '$env/static/private';
 import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals }) => {
-	const session = await locals.auth.validate()
+	const session = await locals.auth.validate();
 
-	console.log(session);
 	if (session) {
-		const getUserInfo = await fetch(`${API_URL}/user/info?user_id=${session}`, {
+		const getUserInfo = await fetch(`${API_URL}/user/info?user_id=${session.user.userId}`, {
 			headers: {
 				Authorization:
 					'Bearer ZGVf1sBBw46sB9l8L0BaEJhJUFT0jY9fm7ztodhgDE3kF3DUyKqK1zgoXBmzXrl1lLYpm059htoWSqYp'
