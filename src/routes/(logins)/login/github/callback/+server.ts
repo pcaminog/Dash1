2ba4a47b38775cb69c819163ad856dc630ca81d0
Pablo Camino <pablo@cloudflare.com>
2023-12-04 14:53:48 -0600

    s

--- src/routes/(logins)/login/github/callback/+server.ts
+++ src/routes/(logins)/login/github/callback/+server.ts
@@ -87,7 +87,7 @@ export const GET = async ({ url, cookies, locals, platform }) => {
 		return new Response(null, {
 			status: 302,
 			headers: {
-				Location: '/home'
+				Location: `${message.account_id}/home`
 			}
 		});
 	} catch (e) {
