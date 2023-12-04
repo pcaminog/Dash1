b3501ed7cf2eee93a9a950ccde696ab3b7727d06
Pablo Camino <pablo@cloudflare.com>
2023-12-04 14:47:41 -0600

    s

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,7 +1,17 @@
-export const load = async ({ locals }) => {
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ locals, url }) => {
 	const session = await locals.auth.validate();
 	if (session) {
-		return { session };
+		if (
+			url.pathname.endsWith('home') ||
+			url.pathname.endsWith('monitor') ||
+			url.pathname.endsWith('settings')
+		) {
+			return { session };
+		} else {
+			redirect(303, `${session.user.accounts.account_id}/home`);
+		}
 	}
 	return;
 };
