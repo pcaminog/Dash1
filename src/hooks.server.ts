2c763fee7100ac6c4a06115b8b3ea3322c626d75
Pablo Camino <pablo@cloudflare.com>
2023-12-05 09:56:16 -0600

    s

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,48 +9,48 @@ export const handle: Handle = async ({ event, resolve }) => {
 	}
 
 	const auth = initializeLucia(event.platform?.env.DB);
-	event.locals.auth = auth.handleRequest(event);
-
-	event.locals.lucia = auth;
-
-	const session = {
-		user: {
-			email: 'pcaminog@outlook.com',
-			username: 'pcaminog',
-			avatar: 'https://avatars.githubusercontent.com/u/26197615?v=4',
-			name: 'Pablo Camino',
-			account_id: '1383b8f1-e494-42f4-b220-b41eba7b63c1',
-			account_name: 'My Organization',
-			account_plan: 'free',
-			userId: 'r4v64lrlde9frkz'
-		},
-		sessionId: 'te9l8yt56xvnmjvgsxahky4odb3ngl9wutuecmes',
-		activePeriodExpiresAt: '2023-12-06T14:49:12.752Z',
-		idlePeriodExpiresAt: '2023-12-20T14:49:12.752Z',
-		state: 'active',
-		fresh: false
-	};
-
-	event.locals.user = session?.user.userId;
-	event.locals.session = session;
-	return await resolve(event);
 
 	// event.locals.auth = auth.handleRequest(event);
 	// event.locals.lucia = auth;
-	// if (event.locals?.auth) {
-	// 	const session = await event.locals.auth.validate();
-	// 	console.log(session);
-	// 	if (!session) {
-	// 		if (!event.url.pathname.startsWith('/login')) {
-	// 			return new Response(null, {
-	// 				status: 307,
-	// 				headers: { location: '/login?hooks_no_session' }
-	// 			});
-	// 		}
-	// 	}
-	// 	event.locals.user = session?.user.userId;
-	// 	event.locals.session = session;
-	// }
+	// const session = {
+	// 	user: {
+	// 		email: 'pcaminog@outlook.com',
+	// 		username: 'pcaminog',
+	// 		avatar: 'https://avatars.githubusercontent.com/u/26197615?v=4',
+	// 		name: 'Pablo Camino',
+	// 		account_id: '1383b8f1-e494-42f4-b220-b41eba7b63c1',
+	// 		account_name: 'My Organization',
+	// 		account_plan: 'free',
+	// 		userId: 'r4v64lrlde9frkz'
+	// 	},
+	// 	sessionId: 'te9l8yt56xvnmjvgsxahky4odb3ngl9wutuecmes',
+	// 	activePeriodExpiresAt: '2023-12-06T14:49:12.752Z',
+	// 	idlePeriodExpiresAt: '2023-12-20T14:49:12.752Z',
+	// 	state: 'active',
+	// 	fresh: false
+	// };
+
+	// event.locals.user = session?.user.userId;
+	// event.locals.session = session;
+	// return await resolve(event);
+
+	event.locals.auth = auth.handleRequest(event);
+	event.locals.lucia = auth;
+	if (event.locals?.auth) {
+		const session = await event.locals.auth.validate();
+		console.log(session);
+		if (!session) {
+			if (!event.url.pathname.startsWith('/login')) {
+				return new Response(null, {
+					status: 307,
+					headers: { location: '/login?hooks_no_session' }
+				});
+			}
+		}
+		event.locals.user = session?.user.userId;
+		event.locals.session = session;
+	}
+	return await resolve(event);
 };
 
 export const handleError: HandleServerError = ({ error, event }) => {
